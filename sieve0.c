241e437f8b13a8d7180f0c41ae624200dfcc2462
Vesa <90590852+veekooFIN@users.noreply.github.com>
2022-05-02 00:32:25 -0700

    Add files via upload

--- sieve0.c
+++ sieve0.c
@@ -40,6 +40,8 @@ void main() {
     printf("\n%d primes\n", count);
     printf("\nTook %f seconds to execute \n", time_taken);
     printf("\nYour machine is %f times faster than Z80(PL/I)-Sieve-benchmark\n", 14.000/time_taken);
+    printf("Your machine is %f times faster than Motorola 68000(Asm)(8 MHz)-Sieve-benchmark\n", 1.12/time_taken);
+    printf("Your machine is %f times faster than Intel 8086(Asm)(8 MHz)-Sieve-benchmark\n", 1.90/time_taken);
     printf("Your machine is %f times faster than Cray1 supercomputer(Fortran)-Sieve-benchmark\n", 0.110/time_taken);
     printf("Your machine is %f times faster than RPi modelB(C)-Sieve-benchmark\n", 0.0205/time_taken);
     printf("Your machine is %f times faster than Linux-X86-PC/Ryzen7-1700X(C)-Sieve-benchmark\n", 0.000893/time_taken);
